5f3bec38f653ade5e0272aa44f394bb5a1295d66
Zolo <zolo@onezol.com>
2023-04-02 19:34:09 +0800

    reactor: 请求拦截器逻辑重写

--- src/api/_config.ts
+++ src/api/_config.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 import qs from "qs";
 import Message from "@arco-design/web-react/es/Message";
 import {StorageKey} from "@/constant/keys";
-import {StatusCode} from "@/constant/status";
-import {ADMIN_STU_ID} from "@/constant/others";
 import storage from "@/utils/storage";
 
 const myAxios = axios.create({
@@ -12,62 +10,36 @@ const myAxios = axios.create({
 });
 
 myAxios.interceptors.request.use(
-    (config) => {
+    config => {
         const {pathname} = window.location;
-
         const token = storage.getItem(StorageKey.USER_TOKEN);
-        const account = storage.getItem(StorageKey.USER_ACCOUNT);
         if (pathname !== "/login" && !token) {
             window.location.href = "/login";
             return;
         }
-        if (pathname !== "/login") {
-            config.headers["Authorization"] = token;
-            config.headers["StuId"] = account;
-            // config.headers["Fingerprint"] = ADMIN_FINGERPRINT;
-        }
+        config.headers["Authorization"] = token;
         return config;
     },
-    () => {
-        Message.error("网络错误!");
+    error => {
+        return Promise.reject(error);
     }
 );
 
 myAxios.interceptors.response.use(
-    (response) => {
-        const {data, config} = response;
-        // 如果是/login请求且登陆成功,则把token存入localStorage
-        // if (config.url === "/login" && data.code === StatusCode.OK) {
-        //     storage.setItem(StorageKey.USER_TOKEN, data.token);
-        // }
-        // 没有权限则重定向到login页面
-        if (data.code === 10009) {
-            Message.error("您的身份已过期,请重新登录!");
-            setTimeout(() => {
-                // window.location.href = "/login";
-            }, 3000);
-        }
-        return data;
+    response => {
+        return response.data;
     },
-    (error) => {
-        if (!error || !error.response) {
-            Message.error("网络错误!");
-        } else if (error && error.response && error.response.status == "401") {
-            Message.error("您的身份已过期,请重新登录!");
+    error => {
+        // 如果是401，跳转到登录页
+        if (error.response.status === 401) {
+            Message.error("登录已过期，请重新登录");
             setTimeout(() => {
                 window.location.href = "/login";
             }, 3000);
         }
+        return Promise.reject(error);
     }
 );
-
-export const get = async (path: string, data: any): Promise<API.Response> => {
-    return await myAxios.get(path, {params: data})
-}
-
-export const post = async (path: string, data: any): Promise<API.Response> => {
-    return await myAxios.post(path, qs.stringify(data))
-}
 
 export const reqForGet = async (path: string, data: any): Promise<API.Response> => {
     return await myAxios.get(path, {params: data})
